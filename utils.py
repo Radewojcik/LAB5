71d87db09ba8d797247c296f8b2cb4da95400e74
radwoj0155 <280157@student.pwr.edu.pl>
2026-05-05 18:36:11 +0200

    update actions v6

--- utils.py
+++ utils.py
@@ -1,20 +1,21 @@
-'''Example calculator'''
+"""Example calculator"""
+
 
 def add(a: int, b: int) -> int:
-    '''return a + b'''
+    """return a + b"""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
-    '''Return the difference between two numbers.'''
+    """Return the difference between two numbers."""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
-    '''Return the product of two numbers.'''
+    """Return the product of two numbers."""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
-    '''Return the quotient of two numbers.'''
+    """Return the quotient of two numbers."""
     return a / b
